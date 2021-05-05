f12e6396007cd27d0ea7602bdcb7bf65fba9514f
AhmedAhmedFekry <ahmedahmedfekry11305654@gmail.com>
2021-05-05 08:29:57 +0200

    done

--- js/app.js
+++ js/app.js
@@ -5,64 +5,68 @@ class Section {
     last_id=0;
 
     // make the content html for every section created when click on the add button 
-    get seactionHtmlConten(){
+    get sectionConten(){
         return `
         <section id="section${this.last_id}" data-n="section ${this.last_id}" class="active-class">
         <div class="landing__container">
         <h2>Section ${this.last_id}</h2>
-        <p>tttttttttttttttttttttttttttttttttttttttttttttttt</p>
+        <p>section............................................................................................</p>
         </div>
         </section>
         `
     };
     // this function for adding new section 
-    new_section(){
+    new_section_added(){
         this.last_id +=1;
-        document.getElementsByTagName('main')[0].insertAdjacentHTML('beforeend',this.seactionHtmlConten);
+        document.getElementsByTagName('main')[0].insertAdjacentHTML('beforeend',this.sectionConten);
     }
 }// end class 
 
 
 
 // class for navbar 
-class Nav {
+class Navbar1 {
     // menu elment select by id 
-    menuElement = document.getElementById('navbar__list');
+    menu_ele = document.getElementById('navbar__list');
 
     // make the content html for menu__link created when click on the add button 
-    buildMenu(){
-        this.menuElement.innerHTML='';
+    create_menu(){
+        this.menu_ele.innerHTML='';
         document.querySelectorAll('section').forEach(element =>{
-            this.menuElement.insertAdjacentHTML("beforeend",`<li> <a class="menu__link" href="#${element.id}" data-section-id="${element.id}" >${element.dataset.n}</a></li>`);
+            this.menu_ele.insertAdjacentHTML("beforeend",`<li> <a class="menu__link" href="#${element.id}" data-section-id="${element.id}" >${element.dataset.n}</a></li>`);
             
         });
-        this.to_section();
+        this.scroll_to_section();
     }
 
+  
     // funtion to scroll into the section when menu link in the navbar for this section 
-    to_section(){
-        this.menuElement.addEventListener('click',function(event){
+    scroll_to_section(){
+        this.menu_ele.addEventListener('click',function(event){
             event.preventDefault();
             // scroll into section smooth 
-            document.getElementById(event.target.dataset.sectionId).scrollIntoView({block: 'end',behavior:'smooth'});
+            const id = event.target.dataset.sectionId
+            document.getElementById(id).scrollIntoView({block: 'end',behavior:'smooth'});
             // add active class to target section
-            activeClass(event.target.dataset.sectionId);
+            activeClass(id);
         });
     }
 }
+const menu = new Navbar1()
+const ele_top = document.getElementById('scrollToTop');
 const section = new Section();
-const menu = new Nav()
-const TopElement = document.getElementById('scrollToTop');
 
-function new_section(){
-    section.new_section();
-    menu.buildMenu();
+
+
+function new_section_added(){
+    section.new_section_added();
+    menu.create_menu();
 }
 
 // function to scroll to top when click on the button up
 
 function scrollToTop(){
-    TopElement.addEventListener('click', ()=>{
+    ele_top.addEventListener('click', ()=>{
         window.scrollTo({
             top:0
         })
@@ -71,12 +75,12 @@ function scrollToTop(){
 
 // this function check which section show  on screen 
 //  section_on_screen
-function section_on_screen(element,buffer){
-    buffer = typeof buffer ==='undefined' ? 0 : buffer;
-    const b=element.getBoundingClientRect();
-    if (b.top >= buffer && b.left >= buffer &&  b.right <= 
-        ((window.innerWidth || document.documentElement.clientWidth) -buffer) &&
-        b.bottom <= ((window.innerHeight || document.documentElement.clientHeight) -buffer ) ){
+function section_on_screen(ele,buf){
+    buf = typeof buf ==='undefined' ? 0 : buf;
+    const b=ele.getBoundingClientRect();
+    if (b.top >= buf && b.left >= buf &&  b.right <= 
+        ((window.innerWidth || document.documentElement.clientWidth) -buf) &&
+        b.bottom <= ((window.innerHeight || document.documentElement.clientHeight) -buf ) ){
             return true
         } else{
             return false
@@ -99,11 +103,11 @@ window.addEventListener('scroll',()=>{
 
     if (scrollPrecent > 50 ){
         // show scroll top button if the scrollPrecent > 50
-        TopElement.classList.remove('noDisplay');
+        ele_top.classList.remove('noDisplay');
     }
     else {
            // hide scroll top button if the scrollPrecent =< 50 
-        TopElement.classList.add('noDisplay');
+        ele_top.classList.add('noDisplay');
     }
     document.querySelectorAll('section').forEach(element =>{
         if(section_on_screen(element,-200)){
@@ -118,10 +122,9 @@ window.addEventListener('scroll',()=>{
 
 
 // calling this funtion to bluid five  new section 
-section.new_section();
-section.new_section();
-section.new_section();
-section.new_section();
-section.new_section();
-menu.buildMenu()
+section.new_section_added();
+section.new_section_added();
+section.new_section_added();
+section.new_section_added();
+menu.create_menu()
 scrollToTop();
